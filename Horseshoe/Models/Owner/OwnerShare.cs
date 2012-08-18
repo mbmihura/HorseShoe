f8404d6bcfd784bd1dd54e8d1e66c2a9c91b805e
mbmihura <mbmihura@gmail.com>
2012-08-18 10:37:31 -0300

    Adds contructor to #OwnerShare and validation to Percentage property (%<100)

--- Horseshoe/Models/Owner/OwnerShare.cs
+++ Horseshoe/Models/Owner/OwnerShare.cs
@@ -2,12 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Horseshoe.Exceptions;
 
 namespace Horseshoe.Models.Owner
 {
     class OwnerShare
     {
-        Owner owner;
-        double percentage;
+        float percentage;
+        public Owner Owner { get; private set; }
+        public float Percentage
+        {
+            get { return percentage; }
+            set
+            {
+                if (value < 0 || value > 100)
+                    throw new SharePercentageOutOfRangeException();
+                else
+                    percentage = value;
+            }
+        }
+        public OwnerShare(Owner owner, float  percentage)
+        {
+            Owner = owner;
+            Percentage = percentage;
+        }
     }
 }
