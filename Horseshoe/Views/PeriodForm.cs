d6573e4dbe9557420128f83dab435fa4d22686d0
mbmihura <mbmihura@gmail.com>
2012-08-27 08:53:37 -0300

    Creates #PeriodForm. Allows user to work within a period.

---
+++ Horseshoe/Views/PeriodForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Horseshoe.Models;
+
+namespace Horseshoe.Views
+{
+    public partial class PeriodForm : Form
+    {
+        public PeriodForm()
+        {
+            InitializeComponent();
+        }
+
+        private void optPeriodHorses_lbl_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void optBill_lbl_Click(object sender, EventArgs e)
+        {
+            new Billing().ShowDialog();
+        }
+
+    }
+}
